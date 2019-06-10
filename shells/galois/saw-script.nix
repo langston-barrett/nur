096ab713686368d6417e4939da8cacbbe83446e5
Langston Barrett <langston.barrett@gmail.com>
2019-06-10 09:09:26 -0700

    shells/galois/saw-script: non-haskell inputs (yices, z3)

--- shells/galois/saw-script.nix
+++ shells/galois/saw-script.nix
@@ -1,4 +1,9 @@
 import ../generic-haskell.nix {
   name = "saw-script";
   additionalHaskellInputs = hpkgs: with hpkgs; [ alex happy ];
+  additionalInputs = pkgs: unstable: with pkgs; [
+    (pkgs.callPackage ../../galois-haskell-nix/pkgs/abc.nix { })
+    unstable.yices
+    z3
+  ];
 }
